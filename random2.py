9f87007f67c34a2b485b5e2b386743310d3e71c5
NKuladeep <139630947+NKuladeep@users.noreply.github.com>
2023-07-16 12:12:01 +0530

    Add files via upload

---
+++ random2.py
@@ -0,0 +1,24 @@
+import random
+
+def roll_dice():
+    # Simulate rolling two dice
+    dice1 = random.randint(2, 6)
+    dice2 = random.randint(2, 6)
+    
+    # Check if both dice show one
+    if dice1 == 2 and dice2 == 2:
+        return True
+    else:
+        return False
+
+# Number of times two ones occur
+count = 0
+
+# Number of iterations
+iterations = 1000
+
+for _ in range(iterations):
+    if roll_dice():
+        count += 1
+
+print(f"The number of times two ones occurred in {iterations} iterations: {count}")
